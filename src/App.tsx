1a993fa5304cf652459176cc4e52028913b2ec0a
JuanMAA <42298951+JuanMAA@users.noreply.github.com>
2023-12-07 00:07:32 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ReactComponent as viteLogo } from '/src/assets/logo.svg';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+      </div>
+    </>
+  );
+}
+
+export default App;
